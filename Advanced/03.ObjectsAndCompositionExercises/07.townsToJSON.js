83e44099de28841ab7cd84642d7cdba9925c740f
vladinSon <vladinson009@icloud.com>
2022-10-08 01:36:42 +0200

    homework

--- Advanced/03.ObjectsAndCompositionExercises/07.townsToJSON.js
+++ Advanced/03.ObjectsAndCompositionExercises/07.townsToJSON.js
@@ -1,8 +1,6 @@
 function townsToJSON(input) {
-    const reg = new RegExp(/[^ | ][A-Za-z\d\s\.]+[^ | $]/, 'g')
+    const reg = new RegExp(/[^ | ][A-Za-z\d\s\.]+[^ | $]/, 'g');
     const result = [];
-
-
     for (let i = 1; i < input.length; i++) {
         let regex = input[i].match(reg);
         const currentTown = {
@@ -11,13 +9,10 @@ function townsToJSON(input) {
             Longitude: Number(Number(regex[2]).toFixed(2))
         }
         result.push(currentTown);
-
     }
     console.log(JSON.stringify(result));
-
 }
-
 townsToJSON(['| Town | Latitude | Longitude |',
     '| Veliko Turnovo ovo | 43.0757 | 25.6172 |',
     '| Monatevideo | 34.50 | 56.11 |'
-])
+]);
