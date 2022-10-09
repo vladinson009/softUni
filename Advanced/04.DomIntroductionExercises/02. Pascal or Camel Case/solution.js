366cd37b4fcf0d16c7252e9270916d6e8b8454f1
vladinSon <vladinson009@icloud.com>
2022-10-09 14:02:10 +0200

    homework

--- Advanced/04.DomIntroductionExercises/02. Pascal or Camel Case/solution.js
+++ Advanced/04.DomIntroductionExercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  const convention = document.getElementById('naming-convention').value;
+  text = text.split(' ');
+  const result = [];
+
+  if (convention == 'Camel Case') {
+    result.push(text[0][0].toLowerCase(), text[0].slice(1).toLowerCase());
+    for (let i = 1; i < text.length; i++) {
+      result.push(text[i][0].toUpperCase() + text[i].slice(1).toLowerCase());
+    }
+  } else if (convention == 'Pascal Case') {
+    result.push(text[0][0].toUpperCase(), text[0].slice(1).toLowerCase());
+    for (let i = 1; i < text.length; i++) {
+      result.push(text[i][0].toUpperCase() + text[i].slice(1).toLowerCase());
+    }
+  } else {
+    result.push('Error!');
+  }
+  document.getElementById('result').textContent = result.join('');
 }
