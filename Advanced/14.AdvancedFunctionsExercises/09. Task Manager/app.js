ee9fb124b43534f605c080a01264732436d11ae1
vladinSon <vladinson009@icloud.com>
2023-03-14 21:08:22 +0100

    homework

--- Advanced/14.AdvancedFunctionsExercises/09. Task Manager/app.js
+++ Advanced/14.AdvancedFunctionsExercises/09. Task Manager/app.js
@@ -1,3 +1,58 @@
 function solve() {
-    console.log("//TODO")
+    document.addEventListener('click', add);
+
+    function add(e) {
+        e.preventDefault();
+        if (e.target.tagName == 'BUTTON') {
+            if (e.target.className == 'green') {
+                const article = e.target.parentElement.parentElement;
+                const buttonsHolder = e.target.parentElement;
+                const inProggress = document.getElementById('in-progress');
+                inProggress.appendChild(article);
+                article.querySelector('.green').remove();
+                const thirdBtn = document.createElement('button');
+                thirdBtn.className = 'orange';
+                thirdBtn.textContent = 'Finish';
+                buttonsHolder.appendChild(thirdBtn);
+            } else if (e.target.className == 'red') {
+                e.target.parentElement.parentElement.remove();
+            } else if (e.target.className == 'orange') {
+                const toGreenSection = document.querySelector('h1.green').parentElement.parentElement.children[1];
+                const removeBtns = e.target.parentElement;
+                toGreenSection.appendChild(removeBtns.parentElement);
+                removeBtns.remove();
+            } else {
+                const [task, date] = document.querySelectorAll('input[type = "text"]');
+                const description = document.getElementById('description');
+                if (task.value != '' && date.value != '' && description.value != '') {
+                    const openPush = document.querySelector('h1[class="orange"]').parentElement.parentElement.children[1];
+                    const article = document.createElement('article');
+                    const h3Task = document.createElement('h3');
+                    const pDescr = document.createElement('p');
+                    const pDate = document.createElement('p');
+                    const divBtn = document.createElement('div');
+                    const firstBtn = document.createElement('button');
+                    const secondBtn = document.createElement('button');
+                    h3Task.textContent = task.value;
+                    pDescr.textContent = `Description: ${description.value}`;
+                    pDate.textContent = `Due Date: ${date.value}`;
+                    divBtn.className = 'flex';
+
+                    firstBtn.className = 'green';
+                    firstBtn.textContent = 'Start';
+                    secondBtn.className = 'red';
+                    secondBtn.textContent = 'Delete';
+
+                    article.appendChild(h3Task);
+                    article.appendChild(pDescr);
+                    article.appendChild(pDate);
+                    article.appendChild(divBtn);
+                    divBtn.appendChild(firstBtn);
+                    divBtn.appendChild(secondBtn);
+                    openPush.appendChild(article);
+                    [task.value, date.value, description.value] = ['', '', '']
+                }
+            }
+        }
+    }
 }
