4e45eeb95080f80c796da6d88fb5ebf0b7575251
vladinSon <vladinson009@icloud.com>
2024-08-29 21:30:05 +0200

    03.Students homework

--- JS_Application/03.data-authentication-exercise/02.Phonebook/app.js
+++ JS_Application/03.data-authentication-exercise/02.Phonebook/app.js
@@ -1,5 +1,77 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+  const loadBtn = document.getElementById('btnLoad');
+  const createBtn = document.getElementById('btnCreate');
+  const ulPhonebook = document.getElementById('phonebook');
+  const url = 'http://localhost:3030/jsonstore/phonebook';
 
+  loadBtn.addEventListener('click', getContacts);
+  createBtn.addEventListener('click', onCreate);
+  ulPhonebook.addEventListener('click', onDelete);
+
+  getContacts();
+
+  async function getContacts(ev) {
+    if (ev) {
+      ev.preventDefault();
+    }
+    ulPhonebook.replaceChildren();
+
+    const response = await fetch(url);
+    const data = Object.values(await response.json());
+
+    data.forEach((el) => {
+      const li = document.createElement('li');
+      const deleteBtn = document.createElement('button');
+
+      li.textContent = `${el.person}: ${el.phone}`;
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.id = el._id;
+
+      li.appendChild(deleteBtn);
+      ulPhonebook.appendChild(li);
+    });
+  }
+  async function onDelete(ev) {
+    ev.preventDefault();
+    const target = ev.target;
+    if (target.tagName == 'BUTTON' && target.textContent == 'Delete') {
+      const options = {
+        method: 'delete',
+      };
+      const response = await fetch(url + `/${target.id}`, options);
+      const data = await response.json();
+      getContacts();
+      return data;
+    }
+  }
+  async function onCreate(ev) {
+    ev.preventDefault();
+    const personInput = document.getElementById('person');
+    const phoneInput = document.getElementById('phone');
+
+    if (personInput.value == '' || phoneInput.value == '') {
+      return;
+    }
+
+    const newPerson = {
+      person: personInput.value,
+      phone: phoneInput.value,
+    };
+    personInput.value = '';
+    phoneInput.value = '';
+
+    const options = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newPerson),
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    getContacts();
+    return data;
+  }
+}
 attachEvents();
--- JS_Application/03.data-authentication-exercise/03.Students/app.js
+++ JS_Application/03.data-authentication-exercise/03.Students/app.js
@@ -1,1 +1,64 @@
-console.log('TODO...');
+const url = 'http://localhost:3030/jsonstore/collections/students';
+const submitBtn = document.getElementById('submit');
+const results = document.getElementById('results');
+const [firstName, lastName, facultyNumber, grade] =
+  document.querySelectorAll('.inputs input');
+
+const tbody = document.createElement('tbody');
+results.appendChild(tbody);
+
+submitBtn.addEventListener('click', onSubmit);
+
+async function onSubmit(ev) {
+  ev.preventDefault();
+  if (
+    firstName.value == '' ||
+    lastName.value == '' ||
+    facultyNumber.value == '' ||
+    grade.value == '' ||
+    isNaN(facultyNumber.value) == true ||
+    isNaN(grade.value) == true
+  ) {
+    return;
+  }
+
+  const newBody = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    facultyNumber: facultyNumber.value,
+    grade: grade.value,
+  };
+  firstName.value = '';
+  lastName.value = '';
+  facultyNumber.value = '';
+  grade.value = '';
+
+  const options = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBody),
+  };
+
+  const response = await fetch(url, options);
+  const newData = await response.json();
+  getCollection();
+}
+
+async function getCollection() {
+  tbody.replaceChildren();
+  const response = await fetch(url);
+  const data = await response.json();
+
+  Object.values(data).forEach((el) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th>${el.firstName}</th>
+    <th>${el.lastName}</th>
+    <th>${el.facultyNumber}</th>
+    <th>${el.grade}</th>`;
+    tbody.appendChild(tr);
+  });
+}
+getCollection();
