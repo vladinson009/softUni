712652a993d677efcd4e8aa95ddb805755110dbd
vladinSon <vladinson009@icloud.com>
2024-11-24 14:57:09 +0100

    changed views directory path

--- JS_Back-End/movie-magic/src/configs/handlebarsConfig.js
+++ JS_Back-End/movie-magic/src/configs/handlebarsConfig.js
@@ -3,5 +3,6 @@ import handlebars from 'express-handlebars';
 export default function handlebarsConfig(app) {
   //setup for handlebars
   app.set('view engine', 'hbs');
+  app.set('views', 'src/views');
   app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 }
