f121ecc93ec036d6b3f476bbfec5c7e585a9af80
vladinSon <vladinson009@icloud.com>
2024-11-27 22:44:19 +0100

    make castId filter optional in case of cast without id

--- JS_Back-End/movie-magic/src/services/castService.js
+++ JS_Back-End/movie-magic/src/services/castService.js
@@ -4,7 +4,7 @@ export function createCast(data) {
   return Cast.create(data);
 }
 export function getCastWithout(casts = []) {
-  const castIds = casts.map((cast) => cast.cast._id);
+  const castIds = casts.map((cast) => cast.cast?._id);
   return Cast.find({ _id: { $nin: castIds } });
   //.nin('_id', castIds);
 }
