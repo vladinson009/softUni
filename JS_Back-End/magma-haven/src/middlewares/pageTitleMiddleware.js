4572db63150a4916a779f68fb91927b59fc5ea8d
vladinSon <vladinson009@icloud.com>
2024-12-03 00:08:59 +0100

    added 3 more page titles for dynamic URL

--- JS_Back-End/magma-haven/src/middlewares/pageTitleMiddleware.js
+++ JS_Back-End/magma-haven/src/middlewares/pageTitleMiddleware.js
@@ -2,8 +2,6 @@ const titles = {
   '/': 'Home Page',
   '/volcanoes/catalog': 'Catalog Page',
   '/volcanoes/create': 'Create Page',
-  '/volcanoes/details': 'Details Page',
-  '/volcanoes/edit': 'Edit Page',
   '/volcanoes/search': 'Search',
   '/user/login': 'Login Page',
   '/user/register': 'Register Page',
@@ -13,6 +11,12 @@ export default function pageTitle(req, res, next) {
   const title = titles[req.url];
   if (title != undefined) {
     res.locals.pageTitle = title;
+  } else if (req.url.includes('/volcanoes/details')) {
+    res.locals.pageTitle = 'Details Page';
+  } else if (req.url.includes('/volcanoes/edit')) {
+    res.locals.pageTitle = 'Edit Page';
+  } else if (req.url.includes('/volcanoes/search/filter')) {
+    res.locals.pageTitle = 'Search';
   }
   next();
 }
