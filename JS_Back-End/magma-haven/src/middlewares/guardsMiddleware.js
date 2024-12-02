5968d034d244ebcf25a050a5ba4470742703c748
vladinSon <vladinson009@icloud.com>
2024-12-03 00:08:08 +0100

    redirect to 404 if tries for unauthorized access

--- JS_Back-End/magma-haven/src/middlewares/guardsMiddleware.js
+++ JS_Back-End/magma-haven/src/middlewares/guardsMiddleware.js
@@ -1,12 +1,12 @@
 export function loggedOnly(req, res, next) {
   if (res.locals.isAuthenticated == false) {
-    return res.redirect('/user/login');
+    return res.redirect('/404');
   }
   next();
 }
 export function guestOnly(req, res, next) {
   if (res.locals.isAuthenticated == true) {
-    return res.redirect('/');
+    return res.redirect('/404');
   }
   next();
 }
