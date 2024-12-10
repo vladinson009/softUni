1ceece0e7ea59d34c24e96699b9adcee45571ca6
vladinSon <vladinson009@icloud.com>
2024-12-10 23:17:24 +0100

    front end adding class for errors

--- JS_Back-End/furniture-store/client/src/views/create.js
+++ JS_Back-End/furniture-store/client/src/views/create.js
@@ -1,67 +1,92 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 import { createRecord } from '../api/data.js';
 
-
-const createTemplate = (onSubmit) => html`<div class="row space-top">
+const createTemplate = (onSubmit, errors) => html`<div class="row space-top">
     <div class="col-md-12">
-        <h1>Create New Furniture</h1>
-        <p>Please fill all fields.</p>
+      <h1>Create New Furniture</h1>
+      <p>Please fill all fields.</p>
     </div>
-</div>
-<form @submit=${onSubmit}>
+  </div>
+  <form @submit=${onSubmit}>
     <div class="row space-top">
-        <div class="col-md-4">
-            <div class="form-group">
-                <label class="form-control-label" for="new-make">Make</label>
-                <input class="form-control valid" id="new-make" type="text" name="make">
-            </div>
-            <div class="form-group has-success">
-                <label class="form-control-label" for="new-model">Model</label>
-                <input class="form-control" id="new-model" type="text" name="model">
-            </div>
-            <div class="form-group has-danger">
-                <label class="form-control-label" for="new-year">Year</label>
-                <input class="form-control" id="new-year" type="number" name="year">
-            </div>
-            <div class="form-group">
-                <label class="form-control-label" for="new-description">Description</label>
-                <input class="form-control" id="new-description" type="text" name="description">
-            </div>
+      <div class="col-md-4">
+        <div class="form-group">
+          <label class="form-control-label" for="new-make">Make</label>
+          <input class="form-control ${errors?.make}" id="new-make" type="text" name="make" />
         </div>
-        <div class="col-md-4">
-            <div class="form-group">
-                <label class="form-control-label" for="new-price">Price</label>
-                <input class="form-control" id="new-price" type="number" name="price">
-            </div>
-            <div class="form-group">
-                <label class="form-control-label" for="new-image">Image</label>
-                <input class="form-control" id="new-image" type="text" name="img">
-            </div>
-            <div class="form-group">
-                <label class="form-control-label" for="new-material">Material (optional)</label>
-                <input class="form-control" id="new-material" type="text" name="material">
-            </div>
-            <input type="submit" class="btn btn-primary" value="Create" />
+        <div class="form-group has-success">
+          <label class="form-control-label" for="new-model">Model</label>
+          <input class="form-control ${errors?.model}" id="new-model" type="text" name="model" />
         </div>
+        <div class="form-group has-danger">
+          <label class="form-control-label" for="new-year">Year</label>
+          <input class="form-control ${errors?.year}" id="new-year" type="number" name="year" />
+        </div>
+        <div class="form-group">
+          <label class="form-control-label" for="new-description">Description</label>
+          <input
+            class="form-control ${errors?.description}"
+            id="new-description"
+            type="text"
+            name="description"
+          />
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="form-group">
+          <label class="form-control-label" for="new-price">Price</label>
+          <input
+            class="form-control ${errors?.price}"
+            id="new-price"
+            type="number"
+            name="price"
+          />
+        </div>
+        <div class="form-group">
+          <label class="form-control-label" for="new-image">Image</label>
+          <input class="form-control ${errors?.img}" id="new-image" type="text" name="img" />
+        </div>
+        <div class="form-group">
+          <label class="form-control-label" for="new-material">Material (optional)</label>
+          <input class="form-control" id="new-material" type="text" name="material" />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Create" />
+      </div>
     </div>
-</form>`;
+  </form>`;
 
 export async function createPage(ctx) {
-    ctx.render(createTemplate(onSubmit));
+  ctx.render(createTemplate(onSubmit));
 
-    async function onSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const data = [...formData.entries()].reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+  async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = [...formData.entries()].reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
 
-        if (Object.entries(data).filter(([k, v]) => k != 'material').some(([k, v]) => v == '')) {
-            return alert('Please fill all mandatory fields!');
+    const errors = Object.entries(data)
+      .filter(([k, v]) => k != 'material')
+      .reduce((acc, [k, v]) => {
+        if (v == '') {
+          Object.assign(acc, { [k]: 'is-invalid' });
+        } else {
+          Object.assign(acc, { [k]: 'is-valid' });
         }
+        return acc;
+      }, {});
 
-        data.year = Number(data.year);
-        data.price = Number(data.price);
-        await createRecord(data);
-
-        ctx.page.redirect('/');
+    if (
+      Object.entries(data)
+        .filter(([k, v]) => k != 'material')
+        .some(([k, v]) => v == '')
+    ) {
+      alert('Please fill all mandatory fields!');
+      return ctx.render(createTemplate(onSubmit, errors));
     }
+
+    data.year = Number(data.year);
+    data.price = Number(data.price);
+    await createRecord(data);
+
+    ctx.page.redirect('/');
+  }
 }
