da3b92f59ced141c726e2cf0899dbef01c774388
vladinSon <vladinson009@icloud.com>
2024-12-10 23:19:10 +0100

    created authentication middleware

---
+++ JS_Back-End/furniture-store/server/src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+export default function authMiddleware(req, res, next) {
+  const token = req.header('X-Authorization');
+  if (!token) {
+    return next();
+  }
+  try {
+    const decodedToken = jwt.verify(token, 'secret');
+    req.user = decodedToken;
+    req.isAuthenticated = true;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: err.message });
+  }
+}
